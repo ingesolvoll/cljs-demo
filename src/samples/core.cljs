4464c905c83fd7d6fd79fc2e5fc81eac58e4e9c0
ingesolvoll <inge@semion.ca>
2019-09-30 23:34:01 +0200

    Form sample

--- src/samples/core.cljs
+++ src/samples/core.cljs
@@ -1,6 +1,7 @@
 (ns ^:figwheel-hooks samples.core
   (:require [re-frame.core :as rf]
             [samples.counter :as counter]
+            [samples.forms :as forms]
             [samples.multiplication :as multiplication]
             [reagent.core :as r]))
 
---
+++ src/samples/forms.cljs
@@ -0,0 +1,24 @@
+(ns samples.forms
+  (:require [samples.re-frame :as f]
+            [reagent.core :as r]))
+
+(defn app []
+  [:form {:class "sample-form"}
+   [:div.form-group
+    [:label "Email address"]
+    [:input {:class       "form-control"
+             :type        "email"
+             :placeholder "Enter email"}]
+
+    [:small {:class "form-text text-muted"}
+     "We'll never share your email with anyone else."]]
+
+   [:div.form-group
+    [:label "Password"]
+    [:input {:class       "form-control"
+             :type        "password"
+             :placeholder "Password"}]]
+   [:div {:class "form-group"}
+    [:input {:type "checkbox"}]
+    [:label "Check me out"]]
+   [:button.btn.btn-primary {:type "submit"} "Submit"]])
